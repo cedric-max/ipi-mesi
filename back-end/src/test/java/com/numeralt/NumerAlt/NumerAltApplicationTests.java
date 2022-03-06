f9f33032bcca3ea004f06ee9e83ca9b5a39bec25
seb <sebastien.kemmoun@ipilyon.net>
2022-03-06 19:39:18 +0100

    t

---
+++ back-end/src/test/java/com/numeralt/NumerAlt/NumerAltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numeralt.NumerAlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerAltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
