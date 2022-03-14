1bb3a22438a4fe85a615da30af64e977af6f6cc8
cedric kadima <kadimacedric3@gmail.com>
2022-03-14 23:45:54 +0100

    initite navbar

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+<!--    <router-view/>-->
+<!--      <div>-->
+        <b-navbar type="dark" variant="dark">
+          <b-navbar-nav>
+            <b-nav-item href="/"><router-link to="/">Home</router-link></b-nav-item>
+            <b-nav-item href="/about"><router-link to="/about">About</router-link></b-nav-item>
+            <!-- Navbar dropdowns -->
+<!--            <b-nav-item-dropdown text="Lang" right>-->
+<!--              <b-dropdown-item href="#">EN</b-dropdown-item>-->
+<!--              <b-dropdown-item href="#">ES</b-dropdown-item>-->
+<!--              <b-dropdown-item href="#">RU</b-dropdown-item>-->
+<!--              <b-dropdown-item href="#">FA</b-dropdown-item>-->
+<!--            </b-nav-item-dropdown>-->
+          </b-navbar-nav>
+        </b-navbar>
+      </div>
+<!--  </div>-->
 </template>
 
 <style>
@@ -17,16 +32,16 @@
   color: #2c3e50;
 }
 
-#nav {
+b-nav-item  {
   padding: 30px;
 }
 
-#nav a {
+b-nav-item  {
   font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+b-nav-item  a.router-link-exact-active {
   color: #42b983;
 }
 </style>
