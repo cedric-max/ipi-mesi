9162a8119cc16c8aade70065c0a4656de3c4565e
cedric kadima <kadimacedric3@gmail.com>
2022-03-29 12:45:03 +0200

    banding call axios offer ok

--- front-end/src/components/OfferCard.vue
+++ front-end/src/components/OfferCard.vue
@@ -1,110 +1,44 @@
 <template>
   <div>
     <div class="container">
-      <div class="card">
-        <div class="card__header">
-          <img
-            alt="card__image"
-            class="card__image"
-            src="https://source.unsplash.com/600x400/?computer"
-            width="600">
-        </div>
-        <div class="card__body">
-          <span class="tag tag-blue">Technology</span>
-          <h4>What's new in 2022
-            Tech</h4>
-          <p>Lorem ipsum dolor sit amet
-            consectetur adipisicing
-            elit. Sequi perferendis
-            molestiae non nemo
-            doloribus. Doloremque,
-            nihil! At ea atque
-            quidem!</p>
-        </div>
-        <div class="card__footer">
-          <div class="user">
-            <img
-              alt="user__image"
-              class="user__image"
-              src="https://i.pravatar.cc/40?img=1">
-            <div class="user__info">
-              <h5>Jane Doe</h5>
-              <small>2h ago</small>
+      <ul id="example-1" v-for="post in posts"
+          :key="post.id">
+          <div class="card">
+            <div class="card__header">
+              <img
+                alt="card__image"
+                class="card__image"
+                src="https://source.unsplash.com/600x400/?work"
+                width="600">
+            </div>
+            <div class="card__body">
+              <span class="tag tag-red">work</span>
+              <h4>{{ post.titre }} /
+                {{ post.niveau_diplome_vise }}</h4>
+              <p>
+                {{ post.description }}</p>
+            </div>
+            <div class="card__footer">
+              <div class="user">
+                <img
+                  alt="user__image"
+                  class="user__image"
+                  src="https://i.pravatar.cc/40?img=2">
+                <div class="user__info">
+                  <h5>{{
+                      post.nom_entreprise
+                    }} / {{
+                      post.ville
+                    }}</h5>
+                  <small>{{
+                      post.date_fin_validite
+                    }}</small>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card__header">
-          <img
-            alt="card__image"
-            class="card__image"
-            src="https://source.unsplash.com/600x400/?food"
-            width="600">
-        </div>
-        <div class="card__body">
-          <span class="tag tag-brown">Food</span>
-          <h4>Delicious Food</h4>
-          <p>Lorem ipsum dolor sit amet
-            consectetur adipisicing
-            elit. Sequi perferendis
-            molestiae non nemo
-            doloribus. Doloremque,
-            nihil! At ea atque
-            quidem!</p>
-        </div>
-        <div class="card__footer">
-          <div class="user">
-            <img
-              alt="user__image"
-              class="user__image"
-              src="https://i.pravatar.cc/40?img=2">
-            <div class="user__info">
-              <h5>Jony Doe</h5>
-              <small>Yesterday</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card__header">
-          <img
-            alt="card__image"
-            class="card__image"
-            src="https://source.unsplash.com/600x400/?car,automobile"
-            width="600">
-        </div>
-        <div class="card__body">
-          <span class="tag tag-red">Automobile</span>
-          <h4>Race to your heart
-            content</h4>
-          <p>Lorem ipsum dolor sit amet
-            consectetur adipisicing
-            elit. Sequi perferendis
-            molestiae non nemo
-            doloribus. Doloremque,
-            nihil! At ea atque
-            quidem!</p>
-        </div>
-        <div class="card__footer">
-          <div class="user">
-            <img
-              alt="user__image"
-              class="user__image"
-              src="https://i.pravatar.cc/40?img=3">
-            <div class="user__info">
-              <h5>John Doe</h5>
-              <small>2d ago</small>
-            </div>
-          </div>
-        </div>
-      </div>
+      </ul>
     </div>
-    <ul id="example-1">
-      <li v-for="post in posts" :key="post.id">
-        {{ post }}
-      </li>
-    </ul>
   </div>
 </template>
 
